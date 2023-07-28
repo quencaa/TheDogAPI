182858ff2c97e9f69df50a75760c65196b98fb3d
Gustavo Teixeira <guteixeira@deloitte.pt>
2023-07-28 13:17:16 +0100

    starting point

---
+++ TheDogAPI/TheDogAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheDogAPI
+//
+//  Created by Gustavo Quenca on 28/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
